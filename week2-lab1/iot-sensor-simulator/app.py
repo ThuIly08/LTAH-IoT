b55f53db269030791c5ec730b30a97d44398f4e0
ThuIly08 <thu.htm@teko.vn>
2026-06-10 18:18:02 +0700

    first commit

---
+++ week2-lab1/iot-sensor-simulator/app.py
@@ -0,0 +1,14 @@
+import json
+import os
+import random
+import time
+import datetime 
+import datetime, timezone
+
+import paho.mqtt.client as mqtt
+
+MQTT_BROKER = os.getenv('MQTT_BROKER', 'localhost') #địa chỉ của mqtt_broker: mặc định là localhost
+MQTT_PORT = int(os.getenv('MQTT_PORT', '1883'))
+MQTT_TOPIC = os.getenv('MQTT_TOPIC','iot/sensors')
+DEVICE_ID = os.getenv('DEVICE_ID', 'sensor-01')
+PUBLIST_INTERVAL = float(os.getenv('PUBLISH_INTERVAL', '2'))
